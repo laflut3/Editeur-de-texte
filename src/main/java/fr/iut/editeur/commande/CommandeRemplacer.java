c237bee98b90ecde8a6e4f47acf5e71beae0ce6d
torresL <leo.torres@etu.umontpellier.fr>
2023-11-06 09:55:30 +0100

    ajout de constructeur dans remplacer

--- src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -3,5 +3,8 @@ package fr.iut.editeur.commande;
 import fr.iut.editeur.document.Document;
 
 public class CommandeRemplacer extends CommandeDocument {
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
 
 }
